3dbcea3402a338d7ccfdfec9ddef4da9c8e2591b
jinoo7099 <vxc7099@gmail.com>
2021-01-15 16:41:30 +0900

    [ADD] : Day5 디버깅

--- checker.js
+++ checker.js
@@ -3,35 +3,36 @@
 // 3. 조사식을 통하여 현재 사용되는 변수의 상태 확인
 // 4. Debug Console을 이용하여 순간적인 함수 값 계산
 // 5. 원하지 않는 값 발견시 해당 부분을 맞는 값으로 처리
+var _ = require("lodash");
 
 class Checker {
-
     constructor(msg) {
         this.msg = msg;
     }
 
     bindMembers(names) {
-        const context = this
+        const context = this;
 
         return function (ourStudents) {
-            return new Promise((resolve) => {
-                //반드시 10밀리세컨드 지연실행되어야만 함              
+            return new Promise(resolve => {
+                //반드시 10밀리세컨드 지연실행되어야만 함
                 setTimeout(function () {
                     const bMember = includeMembers(names, ourStudents);
-                    bMember ? resolve(context.msg["ok"].msg) : resolve(context.msg["fail"].msg)
-                }, 10)
-            })
-        }
+                    bMember ? resolve(context.msg["ok"].msg) : resolve(context.msg["fail"].msg);
+                }, 10);
+            });
+        };
     }
-
 }
 
 //lodash의 every 메서드를 활용해서 구현
 const includeMembers = function (names, ourStudents) {
-}
+    return _.every(names, (a) => ourStudents.includes(a));
+};
 
 const runner = function (generator, names, ourStudents) {
-    const gener = generator(names, ourStudents)
-}
+    const gener = generator(names, ourStudents);
+    gener.next();
+};
 
-module.exports = { Checker, runner }
+module.exports = { Checker, runner };
--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const checker = new a.Checker(msg);
 function* run(source, targets) {
     const rightMsg = msg.ok.msg;
     const checkMember = checker.bindMembers(targets);
-    result = yield checkMember(source);
-    result.then((value) => {
+    result = checkMember(source);
+    yield result.then((value) => {
         assert.equal(value, rightMsg);
         console.log("error없이 프로그래밍이 실행됐습니다");
     }).catch((e) => console.log(`이크 에러가 발생했어요. ${e.message}`));
