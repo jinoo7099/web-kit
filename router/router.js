a25edb36c8e082f59631d51ef8920ef218439f1b
jinoo7099 <vxc7099@gmail.com>
2021-01-27 16:15:33 +0900

    [ADD] : Day9 회원정보 관리 구현
    - 회원목록에서 회원 삭제하는 기능 추가

--- router/router.js
+++ router/router.js
@@ -4,9 +4,9 @@ const Client = require('../models/client.js');
 
 let data = { cnt: 1 };
 router.get('/', (req, res) => {
-  // res.render('index.html');
   Client.find(function (err, clients) {
     if (err) return res.status(500).send({ error: 'read fail' });
+    console.log(clients);
     res.render('index.html', { clients: clients });
   });
 });
@@ -14,24 +14,28 @@ router.get('/', (req, res) => {
 router.post('/create', (req, res) => {
   let client = new Client(req.body);
 
-  console.log(req.body);
-
   client.save(function (err) {
     if (err) {
-      console.error(err);
       console.log('error save');
       return;
     }
     console.log('success save');
+    res.redirect("/");
+
   });
 });
 
-router.delete('/delete', (req, res) => {
-  Client.remove({ _id: req.params.id }, function (err) {
-    if (err) {
+router.get('/delete/:id', (req, res) => {
+  Client.remove({_id:req.params.id}, function(err) {
+    if(err){
       console.log(err);
     }
-  });
+    else{
+      console.log("Client deleted");
+
+      res.redirect("/");
+    }
+  })
 });
 
 module.exports = router;
