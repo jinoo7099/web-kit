29c9fc90488bd3c797e552715c61652a0cf5582e
vxc79 <vxc79@naver.com>
2021-02-08 10:53:15 +0900

    [Fix] : 리펙토링

--- Client/frontServer/public/controller/login.controller.js
+++ Client/frontServer/public/controller/login.controller.js
@@ -1,3 +1,77 @@
+document.addEventListener("click", (e) => {
+  switch (e.target.className) {
+    case "login-btn":
+      loginProcess();
+      break;
+    case "register-btn":
+      registerProcess();
+      break;
+  }
+});
+
+function loginProcess() {
+  const id = $(".input-id").val();
+  const pwd = $(".input-pwd").val();
+  const user = { email: id, password: pwd };
+
+  if (id === "" || pwd === "") {
+    alert("아이디나 비밀번호를 입력해주세요.");
+    return;
+  }
+  fetch("http://127.0.0.1:3000/api/users/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(user),
+    credentials: "include",
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      if (data.loginSuccess === false) {
+        // window.location.reload();
+        $(".input-id").val(null);
+        $(".input-pwd").val(null);
+        alert("다시 입력해주세요..");
+        throw new Error("아이디, 비밀번호 오류");
+      }
+
+      location.href = "/#plan";
+    })
+    .catch((err) => console.log(err));
+}
+
+function registerProcess() {
+  const id = $(".input-id").val();
+  const pwd = $(".input-pwd").val();
+  const user = { email: id, password: pwd };
+
+  if (id === "" || pwd === "") {
+    alert("아이디나 비밀번호를 입력해주세요.");
+    return;
+  }
+
+  fetch("http://127.0.0.1:3000/api/users/register", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(user),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.success === false) {
+        alert("회원가입 실패");
+        throw new Error("회원가입 실패");
+      }
+
+      window.location.reload();
+      alert("회원가입 완료!");
+    })
+    .catch((err) => console.log(err));
+}
+
 function loginPage() {
   $(".app-root").html(`<h1>로그인</h1>
     <div class="login-input-wrap">
--- Client/frontServer/public/controller/plan.controller.js
+++ Client/frontServer/public/controller/plan.controller.js
@@ -22,16 +22,7 @@ $(".app-root").on("click", ".create-btn", function (event) {
 
 $(".app-root").on("click", ".delete-btn", function (event) {
   event.preventDefault();
-  // const root = document.querySelector(".app-root");
-  // root.removeChild(event.target.parentElement);
-  // console.log(event.target.parentElement);
-
-  const name = $(event.target.parentElement).children(".project-btn").html();
-  const master = $(event.target.parentElement)
-    .children(".plan-span-master")
-    .html();
-
-  const deletedPlan = { name: name, master: master };
+  const deletedPlan = getDeletedPlan(event);
   $(event.target.parentElement).remove();
 
   fetch("http://127.0.0.1:3000/api/plan/delete", {
@@ -43,8 +34,16 @@ $(".app-root").on("click", ".delete-btn", function (event) {
   }).then((res) => console.log(res));
 });
 
-// Model
-// plan 페이지 처음 들어왔을때 api 서버로부터 데이터 받아서 클라이언티에서 뷰
+function getDeletedPlan(event) {
+  const name = $(event.target.parentElement).children(".project-btn").html();
+  const master = $(event.target.parentElement)
+    .children(".plan-span-master")
+    .html();
+
+  const deletedPlan = { name: name, master: master };
+  return deletedPlan;
+}
+
 function planPage() {
   // 사용자 인증
   fetch("http://127.0.0.1:3000/api/users/auth", {
@@ -53,36 +52,45 @@ function planPage() {
   })
     .then((response) => response.json())
     .then((data) => {
-      renderPlanPage(data);
-    });
+      if (!data.isAuth) {
+        location.href = "/#";
+        throw new Error("인증 실패");
+      }
+
+      requestPlanPageData(data);
+    })
+    .catch((err) => console.log(err));
 }
 
-function renderPlanPage(data) {
-  if (!data.isAuth) {
-    console.log("인증 실패");
-    location.href = "/#";
-    return;
-  }
-
+function requestPlanPageData(data) {
   fetch("http://127.0.0.1:3000/api/plan", {
     method: "POST",
     credentials: "include",
   })
     .then((res) => res.json())
     .then((data) => {
-      $(".app-root").html(`<h2> Prdasdasoject </h2>
+      renderPlanPage(data);
+    })
+    .catch((err) => console.log(err));
+}
+
+//view
+function renderPlanPage(data) {
+  $(".app-root").html(`<h2> Prdasdasoject </h2>
       <div class="create-input-wrap">
       <input class="input-plan" type="text" name="plan" placeholder="plan">
       </div>
       <button class="create-btn">create</button>`);
 
-      for (let i = 0; i < data.length; i++) {
-        $(".app-root").append(`<div class="project">
+  for (let i = 0; i < data.length; i++) {
+    $(".app-root").append(`<div class="project">
         <button class="project-btn">${data[i].name}</button>
         <button class="delete-btn">delete</button>
-        master : <span class="plan-span-master">${data[i].master}</span>    <span>${data[i].users}</span> <span>${data[i].date}</span> <span>${data[i].state}</span>
+        master : <span class="plan-span-master">${data[i].master}</span>
+        users : <span>${data[i].users}</span> 
+        <span>${data[i].date}</span>
+        <span>${data[i].state}</span>
         </div>`);
-      }
-    });
+  }
 }
 export { planPage };
