009cab851b701a13633a8397d719c102f1cccee4
jinoo7099 <jinoo7099@gmail.com>
2021-02-16 16:55:22 +0900

    [Add] : 기본 폴더  구조

---
+++ airbnb/src/components/App.js
@@ -0,0 +1,27 @@
+import React from 'react'
+// import { useMediaQuery } from 'react-responsive'
+
+function App() {
+  // const isPcOne = useMediaQuery({
+  //   query: '(min-width:1440px)',
+  // })
+  // const isPcTwo = useMediaQuery({
+  //   query: '(min-width:1128px) and (max-width : 1439px) ',
+  // })
+
+  // const isTablet = useMediaQuery({
+  //   query: '(min-width:743px) and (max-width: 1127px)',
+  // })
+
+  // const isMobile = useMediaQuery({
+  //   query: '(max-width: 742px)',
+  // })
+
+  return (
+    <div>
+      <h1>Wasssup</h1>
+    </div>
+  )
+}
+
+export default App
